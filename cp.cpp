6d99412338e13a7fe5c11f6c8318715b8d2cbe70
orifmilod <orifmilod@gmail.com>
2021-06-01 21:23:06 +0200

    Add cp file

---
+++ cp.cpp
@@ -0,0 +1,31 @@
+#include <algorithm>
+#include <functional>
+#include <iostream>
+#include <map>
+#include <numeric>
+#include <set>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+using namespace std;
+#define ll long long int
+
+const ll N = 1'000'000'000'000L;
+
+void solve() {
+  //
+}
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  int n;
+
+  cin >> n;
+  while (n--) {
+    solve();
+  }
+  return 0;
+}
